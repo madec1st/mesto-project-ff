50a1cb9ef416004522d94be5118f9dbc1e944678
Mary Khritanko <madec.1st.87@gmail.com>
2024-03-03 22:10:27 +0300

    работа над шаблонами карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const card = cardTemplate.querySelector('.card').cloneNode(true);
 
 // @todo: DOM узлы
+initialCards.forEach((template) => {
+  card.querySelector('.card__title').textContent = template.name;
+  card.querySelector('.card__image').src = template.link;
+  card.querySelector('.card__image').alt = template.name;
+});
+
 
 // @todo: Функция создания карточки
+function createCard() {
+  const cardsContainer = document.querySelector('.places__list');
+  cardsContainer.append(card);
+
+
+  const deleteButton = card.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', deleteCard);
+}
 
 // @todo: Функция удаления карточки
+function deleteCard() {
+
+}
 
 // @todo: Вывести карточки на страницу
+createCard();
